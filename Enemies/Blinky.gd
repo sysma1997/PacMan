76472b251a20a916dffe644d4fb1e46a9a593e26
Sebastian Moreno Acero <sysma1997@gmail.com>
2022-10-04 23:02:09 -0500

    Add movement to enemy

--- Enemies/Blinky.gd
+++ Enemies/Blinky.gd
@@ -9,12 +9,24 @@ var direction = Vector2()
 var before_direction = Vector2()
 var next_direction = Vector2()
 
+var random: RandomNumberGenerator
+
 func _ready():
 	enemy = Enemy.new()
+	random = RandomNumberGenerator.new()
 	
-	next_direction = Vector2(1, 0)
+	next_direction = Vector2(-1, 0)
 	next_direction = next_direction.normalized() * speed
 
 func _physics_process(delta):
 	direction = move_and_slide(next_direction * delta)
 	enemy.set_animation_direction(direction, $Animations)
+	
+	if direction.length() == 0:
+		before_direction = next_direction
+		next_direction = enemy.set_random_direction(before_direction, speed)
+	
+	var new_direction = random.randi_range(1, 100)
+	if new_direction < 2:
+		print(new_direction)
+		next_direction = enemy.set_random_direction(before_direction, speed)
--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -2,8 +2,33 @@ extends Object
 
 class_name Enemy
 
+enum Direction {
+	UP = 1, 
+	RIGHT = 2, 
+	DOWN = 3
+	LEFT = 4,
+}
+
+var random: RandomNumberGenerator
+
 func _init():
-	pass
+	random = RandomNumberGenerator.new()
+
+func set_random_direction(before_direction: Vector2, speed: int) -> Vector2:
+	var next_direction = Vector2()
+	
+	var direction: int = random.randi_range(1, 4)
+	match direction:
+		Direction.UP:
+			next_direction.y -= 1
+		Direction.RIGHT:
+			next_direction.x += 1
+		Direction.DOWN:
+			next_direction.y += 1
+		Direction.LEFT:
+			next_direction.x -= 1
+	
+	return next_direction.normalized() * speed
 
 func set_animation_direction(direction: Vector2, animations: AnimatedSprite):
 	if direction.x > 1:
